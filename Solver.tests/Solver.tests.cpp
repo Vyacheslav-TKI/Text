fbcd908ab8cfd36390d2587d83229ddb580d1ffa
I461-TKI <lukankinv20@gmail.com>
2024-02-18 17:23:40 +0300

    Добавьте файлы проекта.

---
+++ Solver.tests/Solver.tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Solvertests
+{
+	TEST_CLASS(Solvertests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
